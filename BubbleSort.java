c1c976bcf122606c083ea7278c31b7c55941a8cb
backtodeath <lirolaluis@gmail.com>
2016-08-28 18:57:54 +0300

    checking commit to git

---
+++ BubbleSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * @author backtodeath
+ * @email lirolaluis@gmail.com
+ * @date 28.08.2016
+ **/
+
+class BubbleSort {
+    static void sortWithBubbleSorting(int[] unsortedNumbers) {
+        System.out.println("Starting bubble sorting");
+        System.out.println("array before sorting" + Arrays.toString(unsortedNumbers));
+
+        int[] sortedNumbers = {1};
+
+        System.out.println("array after sorting" + Arrays.toString(sortedNumbers));
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
-public class Main {
+/**
+ * @author backtodeath
+ * @email lirolaluis@gmail.com
+ * @date 28.08.2016
+ **/
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World asd!");
+        System.out.println("Starting application");
+
+
+        int[] unsortedNumbers = {28, 16, 57, 14, 22, 103, -5, 0, 77, 166};
+        BubbleSort.sortWithBubbleSorting(unsortedNumbers);
+
     }
 }
